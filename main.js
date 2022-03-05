6b674fe099b0ce63bc7f68a091edc102bd62bfd7
hoangbd99 <hoangbdhe130226@fpt.edu.vn>
2022-03-05 23:04:34 +0700

    hihi

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // You can change global variables here:
-var radius = 240; // how big of the radius
+var radius = 380; // how big of the radius
 var autoRotate = true; // auto rotate or not
 var rotateSpeed = -60; // unit: seconds/360 degrees
 var imgWidth = 120; // width of images (unit: px)
